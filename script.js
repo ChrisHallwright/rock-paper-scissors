622edacbb2efd6737fab0729f906dd714e36f54f
Chris Hallwright <38396526+ChrisHallwright@users.noreply.github.com>
2026-08-31 21:26:33 +1000

    Play a round, accumulate score and announce round winner

--- script.js
+++ script.js
@@ -9,4 +9,28 @@ function getHumanChoice() {
     return choice.toLowerCase();
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    let msg;
+    if (humanChoice === 'rock' && computerChoice == 'paper'
+        || humanChoice === 'paper' && computerChoice == 'scissors'
+        || humanChoice === 'scissors' && computerChoice == 'rock') {
+        computerScore += 1;
+        msg = `You lose! ${computerChoice} beats ${humanChoice}`;
+    } else if (computerChoice === 'rock' && humanChoice == 'paper'
+        || computerChoice === 'paper' && humanChoice == 'scissors'
+        || computerChoice === 'scissors' && humanChoice == 'rock') {
+        humanScore += 1;
+        msg = `You WIN!! ${humanChoice} beats ${computerChoice}`;
+    } else {
+        msg = `Draw - computer also chose ${computerChoice}`;
+    }
+    console.log(msg);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
